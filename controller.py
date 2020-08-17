2f9c7bbd395f541901b87fabda52971fab8d595f
david <david@dadahasa.com>
2020-08-17 09:35:41 -0500

    Implement mqtt to control relays

---
+++ controller.py
@@ -0,0 +1,384 @@
+
+#This file is spawned by firebase-listener.js
+# https://www.sohamkamani.com/blog/2015/08/21/python-nodejs-comm/
+
+import sys
+from subprocess import call
+from threading import Timer
+from threading import Thread
+import paho.mqtt.client as mqtt
+
+# import RPi.GPIO as GPIO
+import time
+#import spidev
+
+
+# Credentials required by MQTT moquitto broker
+# MQTT Broker is a linux service that should start automatically on boot
+from mqtt_credentials import *
+
+##################################
+# TODO
+##################################
+# set this to primary or backup
+# the two controllers should monitor each other with keepalives
+# and switch roles when detecting a failure
+mqttClientName = "controller"
+
+# siteId is provided by firebase as a reply to a hello message
+siteId = "TEST"
+# nodeId and wanIp are provided by the node on a hello message
+nodeId = "NO_NODEID"
+wanIp = "none"
+
+
+
+mqttTopicMaxLength = 50
+mqttMsgMaxLength = 50
+
+
+
+# Send mqtt commands to node to turn on or off a particular relay
+def relayControl(cmd):
+    if cmd[0] != "SWITCH":
+        return
+    nodeId = cmd[1]
+    ###
+    #togle single relay
+    if cmd[3]=="s": #Single relay command
+        relay = cmd[4] # relay number
+        rlState = 1 if cmd[2]=='true' else 0 #0= Off, 1= On
+        mqttPub(nodeId, "relay/" + str(relay), str(rlState))
+    ###
+    #On and Off push buttons (using two relays for contactors)
+    elif cmd[3] == "p": #Pulse one of the two-relay set on and then off
+        rlState = 1 if cmd[2]=='true' else 0 #0= Off, 1= On
+        relay = cmd[4] if rlState else cmd[5] #on = first relay, off = second relay
+        pulse(nodeId, relay)
+
+#Turns a relay on for 1 second, then off
+def pulse(nodeId, relay):
+    def off(relay):
+        mqttPub(nodeId, "relay/" + str(relay), str(0)) #turn relay off
+    mqttPub(nodeId, "relay/" + str(relay), str(1)) #turn on
+    t = Timer(1, off, [relay])
+    t.start();
+
+
+
+
+
+# callback to get mqtt messages received
+def on_message(client, userdata, message):
+    print("Message received: ")
+    # print("message topic=",message.topic)
+    # print("message qos=",message.qos)
+    # print("message retain flag=",message.retain)
+    topicKeys = parse_topic(message.topic)
+    for i, element in enumerate(topicKeys):
+        print ("key", i, element)
+
+    print ("Payload: ", str(message.payload.decode("utf-8")))
+
+    nodeId = topicKeys[1] #This will be true for all messages published by nodes
+
+    # Handle HELLO message
+    # If WAN IP is associated to a siteId -> send siteId to node and add/update node data in firebase
+    # if WAN IP is unknown -> request node siteId (payload '?')
+    if (str(topicKeys[0]) == 'hello'):
+        nodeId = topicKeys[1]
+        print("HELLO: NODE id " , nodeId)
+        time.sleep(0.1) #delay to allow wifi node to receive mqtt immediately after sending hello
+
+        # VERIFY THE NODE IS SENDING THE WAN IP AS PAYLOAD!!
+        wanIp = str(message.payload.decode("utf-8"))
+        print("wanIp from hello: ", wanIp)
+
+        ##vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+        # QUERY FIREBASE IF WAN IP IS ASSOCIATED TO A SITE ID
+        #use dbSend(key, value)
+        # dbSend('Sent to stdout the WAN IP!', wanIp)
+        ##^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+
+        #SEND THE SITE ID TO THE NODE IF KNOWN, OTHERWISE NODE SHOULD PROMPT USER FOR SITE ID
+        mqttPub(nodeId, 'siteId', 'ABCDEFGHI')
+        # mqttPub(nodeId, 'siteId', '?') #Example to get current setting
+
+        # ONCE THE NODE HAS THE CORRECT SITE ID, IT SHOULD SEND A NEW HELLO TO THE SITEID (CONTROLLER)
+        # THEN THE CONTROLLER SHOULD UPDATE FIREBASE WITH THE NODE STATUS, SETTINGS AND CAPABILITIES
+        # THEN FIREBASE SHOULD UPDATE SETTINGS FOR MASTER, BACKUP AND SITE CONTROLLERS
+        # THEN THE NODE SHOULD TEST ALL CONTROLLERS TO DETERMINE CONNECTIVITY
+
+
+def on_connect(client, userdata, flags, rc):
+    if rc==0:
+        print("connected OK Returned code=",rc)
+    else:
+        print("Bad connection Returned code=",rc)
+
+def on_log(client, userdata, level, buf):
+    print("log: ",buf)
+
+
+#function to split the topic by the '/'delimiter and return a list
+def parse_topic(topic):
+    #list returned will have:
+    #TO (or hello), FROM, target, [index]
+    return topic.split('/')
+
+
+
+# create an instance of the mqtt client
+mqttClient = mqtt.Client(mqttClientName)
+mqttClient.username_pw_set(mqttUser, mqttPswd)
+
+# for testing
+#mqttClient.publish("topic/test", "Hello world!")
+
+#callbacks
+mqttClient.on_message = on_message #attach function to CALLBACK
+mqttClient.on_log = on_log
+mqttClient.on_connect = on_connect
+
+mqttClient.connect("localhost")
+
+mqttClient.loop_start() # starts mqtt callback loop to display published messages
+time.sleep(4)  # time to process the callback
+# mqttClient.subscribe("topic/test")
+# mqttClient.subscribe("esp/test")
+#mqttClient.publish("topic/test", "Hello world!")
+
+### DEFINE THIS ON A SEPARATE HEADER FILE.
+### NEEDS TO BE DIFFERENT FOR THE MASTER AND SITE CONTROLLERS
+### PRIMARY/BACKUP SUBSCRIBE TO EVERYTHING
+### SITE CONTROLLER ONLY SUBSCRIBES TO ITS SITEID
+mqttClient.subscribe("#") #THIS IS ONLY FOR THE MASTER AND BACKUP CONTROLLERS!
+
+
+def mqttPub(nodeId, key, value):
+    #GET: Set value to '?' to GET status/config (stated in the topic) from the nodes
+    #SET: Setting value to any other string will SET the topic parameter to that value.
+    topic = str(nodeId) + '/' + str(key)
+    value = str(value)
+    # print("Topic: " + str(topic) + " - Topic Length: " + str(len(topic)) )
+    # Verify Topic string is not longer than the max the node can accept
+    if len(topic) > mqttTopicMaxLength:
+        print(\
+        "ALERT: mqtt TOPIC (" + str(topic) +") length (" + str(len(topic)) + \
+        ") exceeds node limit (" + str(mqttTopicMaxLength) +  \
+        ") and it will be truncated")
+
+    # Verify payload/value string is not longer than the max the node can accept
+    if len(value) > mqttMsgMaxLength:
+        print(\
+        "ALERT: mqtt PAYLOAD (" + str(value) +") length (" + str(len(value)) + \
+        ") exceeds node limit (" + str(mqttMsgMaxLength) +  \
+        ") and it will be truncated")
+
+        # or len(value) > mqttMsgMaxLength:
+    # call(["mosquitto_pub", "-t", topic, "-u", mqttUser, "-P", mqttPswd, "-m", value], shell = False)
+    mqttClient.publish(topic, value)
+
+def dbSend(key, value):
+    '''
+    function to send data to FIREBASE via stdout to node.js thread
+    '''
+    sys.stdout.flush()
+    sys.stdout.write(key + ":" + str(value))
+    sys.stdout.flush()
+    # needs a small delay to allow time to flush
+    time.sleep(.1)
+
+
+def main():
+    '''
+    This loop runs on a separate thread (dataIn = Thread(target = main, args = ()) )
+    and waits on the readline instruction until it receives something
+    '''
+    while 1:
+        # Interface with the Node.js module is done via stdin messages
+        # read messages from database listener
+        line = sys.stdin.readline()
+        #separate the key/value
+        # status[0] = switch # ID
+        # status[1] = switch state (see swState dictionary for valid replies)
+        status = (line.strip().split(':'))
+
+        #vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+        ## FOR TEST ONLY.
+        ##This will increase the size of a local file (message.txt)
+        ## log the key/value (switch /state) received from firebase
+        f = open("message.txt", "a")
+        for arg in status:
+            f.write(arg)
+            f.write(' : ')
+        f.write("\n")
+        f.close()
+        #^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+        # RELAY Control
+
+        # Check if it is a relay control message
+        if status[0] == "SWITCH":
+
+            # f = open("message.txt", "a")
+            # f.write("Relay Control for ")
+            # f.write(status[1])
+            # f.write("\n")
+            # f.close()
+
+            relayControl(status)
+
+            # nodeId = status[1]
+            # if status[3]=="s": #Single relay command
+            #     relay = status[4] # relay number
+            #     rlState = 1 if status[2]=='true' else 0 #0= Off, 1= On
+            #     mqttPub(status[1], "relay/" + str(relay), str(rlState))
+            #
+            # elif status[3] == "p": #Pulse two relays for on/off
+            #     rlState = 1 if status[2]=='true' else 0 #0= Off, 1= On
+            #     relay = status[4] if rlState else status[5] #on = first relay, off = second relay
+            #     pulse(nodeId, relay)
+
+
+
+
+
+            # mqttPub(status[1], "relay/" + str(relay), str(rlState))
+
+
+        # Using the swState.get to prevent an incorrect return value from causing an exception
+        #setRelay(switches[status[0]], swState.get(status[1], swStateDefault))
+
+        ### For testing function to turn on/off a relay
+        # setRelay(16, 1)
+        # time.sleep(2)
+        # setRelay(16,0)
+
+
+        ##this code shuts down the controller
+        # elif status[0] == SW_5:
+        #     if status[1] != 'false':
+        #          call('sudo /sbin/shutdown -P now', shell = True)
+
+
+# def readInput(channel):
+#     adc = spi.xfer2([1,(8+channel)<<4,0])
+#     data = ((adc[1]&3) << 8) + adc[2]
+#     return data
+#
+#
+# def sendOut(sensorId, value):
+#     sys.stdout.write(sensorId + ":" + value)
+#     sys.stdout.flush()
+#     # needs a small delay to allow time to flush
+#     time.sleep(.1)
+
+
+#----------------------------------------------------
+#----------------------------------------------------
+#----------------------------------------------------
+# SCRIPT STARTS HERE!
+#----------------------------------------------------
+#----------------------------------------------------
+#----------------------------------------------------
+#--------------------------------------
+#--------------------------------------
+#--------------------------------------
+#Start main() in a separate thread to listen for stdin messages from node.js
+dataIn = Thread(target = main, args = ())
+dataIn.start()
+#--------------------------------------
+#--------------------------------------
+#--------------------------------------
+
+# GET HERE THE CURRENT STATUS OF ALL SWITCHES AND STORE THEM IN A dictionary
+# ONLY PUBLISH AN MQTT MESSAGE TO NODE IF THE SWITCH STATE HAS CHANGED
+# OR SHALL WE HAVE THE NODE DO THAT?
+
+
+
+
+## FOR TESTING SENDING MESSAGES TO JAVASCRIPT
+# for x in range (5):
+#     dbSend("SFM", x)
+#     time.sleep(1)
+
+
+## Don't let this script end!!!
+## otherwise it will teminate the spawned instance from node.js, with the error:
+## Error [ERR_STREAM_DESTROYED]: Cannot call write after a stream was destroyed
+while True:
+    time.sleep(1)
+
+
+#GET MQTT MESSAGES FROM NODES AND UPDATE FIREBASE
+#mosquitto_sub -h localhost -p 1883 -t "esp/test" -u sfmNode -P 7VYSDEw92Zs -v
+
+# mqttClient.publish("topic/test", "Hello world!")
+
+# #read SPI sensors
+# spi = spidev.SpiDev()
+# spi.open(0,0)
+# spi.max_speed_hz = 7629
+#
+# numberOfSamples = 3
+# #!!numberOfSamples = 10
+#
+# numberOfSensors = 2
+# previousSensorValue = [-1]*numberOfSensors
+#
+#
+#
+# while True:
+#     readings = []
+#     for n in range(numberOfSamples):
+#         sample = []
+#         for sensor in range(2):
+#             rawValue = (readInput(sensor))
+#
+#             #-----------------------------
+#             # Fore testing Only
+#             #print str(sensor) + " : " + str(rawValue)
+#             #-----------------------------
+#
+#             # convert ADC digital code to amps
+#             #!! DO AMPS CONVERSION JUST ONCE AFTER THE AVERAGING! JUST APPEND SAMPLE RAW VALUES
+#             sample.append(int (round(rawValue * sensorCorrection)))
+#             #!!sample.append(rawValue)
+#
+#         readings.append(sample)
+#         time.sleep(1)
+#         #!!time.sleep(0.2)
+#
+#     # get the average of the readings
+#     sensorAverageValue = [0]*numberOfSensors
+#     samples = len(readings)
+#     for reading in range(samples):
+#         sensors = len(readings[reading])
+#         for sensor in range(sensors):
+#             sensorAverageValue[sensor] += readings[reading][sensor]
+#
+#     for sensor in range(numberOfSensors):
+#         # Get the average
+#         sensorAverageValue[sensor] /= 3
+#         #!!sensorAverageValue[sensor] /= numberOfSamples
+#         #convert to amps
+#         sensorAverageValue[sensor] = int(round(sensorAverageValue[sensor] * sensorCorrection))
+#
+#
+#     #print sensorAverageValue
+#
+#     # send sensor's readings to the node.js listener
+#     for sensor in range(len(sensorAverageValue)):
+#         #print "Channel "+str(i), " : "+ str(amps)
+#
+#         # Only update the database if the value has changed`
+#         if (sensorAverageValue[sensor] != previousSensorValue[sensor]):
+#             previousSensorValue[sensor] = sensorAverageValue[sensor]
+#             sensorId = "c_" + str(sensor+1)
+#             #map sensor to app switch
+#             sensorId = mapSensorToSwitch[sensorId]
+#
+#             value = str(sensorAverageValue[sensor])
+#             sendOut(sensorId, value)
---
+++ firebase_listener.js
@@ -0,0 +1,280 @@
+
+
+//this file is for the raspberry pi node.js
+
+// Import Firebase API keys and controller credentials
+//https://www.stanleyulili.com/node/node-modules-learn-how-to-import-and-use-functions-from-another-file/
+//https://stackoverflow.com/a/7612052
+const {apiKey, authDomain, databaseURL, storageBucket, serviceAccountCredentialsPath,siteId,
+        password, email} = require('./google_services.js')
+
+// Fine site ID for a known WAN IP
+//https://stackoverflow.com/a/46615568
+function findSiteId(siteIp){
+  //returns the siteId for a given WAN IP, or null for unknown IP
+  var sites = database.ref().child('sites');
+  var query = sites.orderByChild("siteConfig/ip/wanIp").equalTo(siteIp);
+  let siteid = 'NONE';
+
+  return query.once('value').then(function(snapshot) {
+    //returns the name of the SITEID
+    snapshot.forEach(function (data){
+      siteid = data.key;
+    });
+    console.log('SITEID FOUND: ' + siteid);
+    return siteid;
+  });
+}
+
+function getSiteId(siteIp){
+  return findSiteId(siteIp).then(function(siteid){
+    console.log('MERDA: ' + siteid);
+  });
+}
+
+
+//import firebase from "firebase"; //for ES6 imports or TypeScript
+var firebase = require("firebase");
+
+//get a handle of the Firebase instance
+config = {
+    "apiKey": apiKey,
+    "authDomain": authDomain,
+    "databaseURL": databaseURL,
+    "storageBucket" : storageBucket,
+    "serviceAccount": serviceAccountCredentialsPath
+}
+firebase.initializeApp(config);
+
+
+// Get a reference to the database service
+var database = firebase.database();
+
+
+//https://firebase.google.com/docs/auth/web/auth-state-persistence
+firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    	// Handle Errors here.
+    	var errorCode = error.code;
+    	var errorMessage = error.message;
+
+    	console.log('Auth Error!');
+    	console.log(errorCode);
+    	console.log(errorMessage);
+});
+
+
+
+firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+            // controller is signed in.
+        	//write a log message
+        	console.log('got autenticated!');
+
+        	var uid = user.uid;
+        	console.log('Authenticated uid: ' + uid);
+
+        	///need to wait to access the database until the user is fully signed in!!!!
+        	//ask stackoverflow why there are no examples of this on the web, about waiting for the
+        	//auth state change before proceeding reading or writing to database!
+
+        	//To write to database
+        	//see this: https://firebase.google.com/docs/database/admin/save-data
+          // var switches = database.ref('sites/' + siteId + "/switches");
+          // var sensors = database.ref("controllers/" + controller + "/sensors");
+
+
+
+          ///vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+          //Example for testing: It adds a new node to firebase
+          var node = "123ABCD"
+          var relays = 16
+          //set database reference on that nodeId
+          var newNode = database.ref('sites/' + siteId + '/nodes/' + node);
+          //update database field
+          //https://stackoverflow.com/questions/33610616/firebase-update-callback-to-detect-error-and-sucssess
+          newNode.update({relays}).then(function(){
+            console.log('Data updated!');
+          }).catch(function(error) {
+            console.log('Update error: ' + error);
+          });
+          ///^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+          //Read data ONCE (this works!)
+          //https://firebase.google.com/docs/database/admin/retrieve-data#section-reading-once
+          // var siteIps = database.ref('siteIps');
+          // siteIps.once("value").then(function (snapshot) {
+          //   console.log("SITEIPS:", snapshot.val());
+          // });
+
+
+          ////HELLO HANDSHAKE
+          // Retrieve siteId with given WAN IP or null if unknown ip
+          // https://stackoverflow.com/a/51614404
+          //https://stackoverflow.com/a/40935483
+          //https://stackoverflow.com/a/40455744
+          // var sites = database.ref().child('sites');
+          // var query = sites.orderByChild("siteConfig/ip/wanIp").equalTo("73.5.142.186");
+          // // var query = sites.orderByChild("siteConfig/ip/wanIp").equalTo("73.5.142.187");
+          // query.once('value').then(function(snapshot) {
+          //   console.log(snapshot.val());
+          //   console.log('SITES URL = ' + snapshot.ref); //prints the sites URL
+          //
+          //   console.log('KEY1 = ' + snapshot.ref.key); //prints the parent object name 'sites'
+          //   console.log('KEY2 = ' + snapshot.key); //prints the parent object name 'sites'
+          //
+          //   // console.log('SITEID = ' + snapshot.ref.child.val); //undefined
+          //   console.log('Project URL= ' + snapshot.ref.parent); // returns the project url
+          //
+          //   //returns the name of the SITEID
+          //   snapshot.forEach(function (data){
+          //     console.log(data.key);
+          //   });
+          // });
+
+          //vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+          // FOR TESTING HELLO PROCESS
+          // console.log('KAKA: ' +  getSiteId("73.5.142.186X") );
+          // console.log('KOKO: ' + getSiteID(siteIp)
+          getSiteId("73.5.142.186");
+          //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+          //remove all sensor data
+          // sensors.remove();
+
+        	//write to the database at the root reference "ref"
+        	//On booting, reset all switches' states of the database to the default off.
+        	// switches.set({
+        	// 	bin_1_sw_1: false,
+          //   bin_1_sw_2: false,
+          //   bin_1_sw_3: false,
+          //   bin_1_sw_4: false,
+          //   bin_1_sw_5: false,
+          //   bin_1_sw_6: false,
+          //   bin_1_sw_7: false,
+          //   bin_1_sw_8: false
+        	// });
+
+          /// GET Private and Public IP addresses
+          var wanIp;
+          var lanIp;
+          //reference for firebase's IP fields
+          var ip = database.ref('sites/' + siteId + '/siteConfig/ip');
+          const { exec } = require('child_process');
+
+          // Get Controller's Public IP
+          // exec('dig +short myip.opendns.com @resolver1.opendns.com', (err, stdout, stderr) => {
+          exec('curl ifconfig.me', (err, stdout, stderr) => {
+            if (err) {
+              //couldn't execute the command
+              return;
+            }
+            //retrieve controller's public IP
+            wanIp = stdout;
+            console.log('wanIp: ' + wanIp);
+            //Update WAN IP to Database
+            ip.update({wanIp}).then(function(){
+              console.log('Data updated!');
+            }).catch(function(error) {
+              console.log('Update error: ' + error);
+            });
+          });
+
+          //Get controller's private IP
+          //"hostname -I | awk '{print $1}'" //this returns only the ipv4
+          exec('hostname -I', (err, stdout, stderr) => {
+            if (err) {
+              // node couldn't execute the command
+              return;
+            }
+            lanIp = stdout;
+            console.log('lanIp: ' + lanIp);
+            ip.update({lanIp}).then(function(){
+              console.log('Data updated!');
+            }).catch(function(error) {
+              console.log('Update error: ' + error);
+            });
+          });
+
+
+
+          //////////////////////////////////////
+          //SPAWN PYTHON CONTROLLER
+          //////////////////////////////////////
+
+          //vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+        	// // spawn python script
+        	var spawn = require("child_process").spawn;
+        	var pyProcess = spawn('python',["controller.py"]);
+          //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+          // THIS CALLBACK IS EXECUTED EVERYTIME A SWITCH IS MOVED
+        	// Attach a listener to get a snapshot with the data that has changed
+        	// and send that data to the python controller
+
+          /////////////////////////////////////////////
+          // GET SNAPSHOT WITH ALL SWITCH Data that changed
+          //https://firebase.google.com/docs/reference/admin/node/admin.database.DataSnapshot
+          /////////////////////////////////////////////
+          /////// SITEID IS HARDCODED! PRI/SEC CONTROLLERS NEED TO LISTEN TO ALL SITEIDS - NEED TO FIX!!!
+          var switches = database.ref('/sites/55347/switches');
+          ////////////////////////////////////////////////
+
+          switches.on('child_changed', function(snapshot) {
+            // //print the switch ID that changed
+            // console.log("Switch: " + snapshot.ref.key);
+            // //Print the values of specific known fields
+            // console.log("NodeID: " + snapshot.child("nodeId").val());
+            // console.log("Switch State: " + snapshot.child("swState").val());
+            // console.log("Switch Logic: " + snapshot.child("swLogic").val());
+
+            ///Send switch status change to python
+            var nodeId = snapshot.child("nodeId").val();
+            var swState = snapshot.child("swState").val();
+            var swLogic = snapshot.child("swLogic").val();
+
+            //The new line character \n at the end is needed to flush the message
+            // pyProcess.stdin.write('NodeID:' + nodeId + '\n');
+            //The first parameter is the comand identifyer. ('SWITCH' = relay control)
+            var message = 'SWITCH:' + nodeId + ':' + swState + ':' + swLogic;
+            console.log("MESSAGE: " + message);
+            pyProcess.stdin.write(message + '\n');
+
+
+            // //get sensor data
+            // pyProcess.stdout.on('data', (data) => {
+            //   // Do something with the data returned from python script
+            //   console.log(data.toString());
+            // });
+          });
+
+
+
+
+          //get sensor data
+          pyProcess.stdout.on('data', (data) => {
+              // Here is the data received from python
+              console.log("RECEIVED FROM CONTROLLER: " + data.toString());
+
+              //get the sensorId:value pairs. remove the end carriage return
+              //var rawValue = data.toString().replace(/\n/g, '')
+              var rawValue = data.toString()
+              //console.log(rawValue)
+              //split id:value
+              var splitIdValue = rawValue.split(":");
+              var sensorId = splitIdValue[0].toString();
+              var sensorValue = splitIdValue[1].toString();
+              console.log(sensorId + " " + sensorValue);
+              // sensors.update({[sensorId]:sensorValue})
+
+              //send to database. Remove the newline character \n
+              //sensors.set({c_1:data.toString().replace(/\n/g, '')})
+          });
+
+        	console.log('Listener is installed!');
+
+      } else {
+        	// User is not signed in.
+        	// Handle Errors here.
+        	console.log(user + ": Not Authorized!");
+      }
+});
